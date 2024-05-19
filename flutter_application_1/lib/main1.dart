4901c168d5be21ec3d53fbaf8a428aea74e87820
azimjon0103 <azimjonfayzullayev3@gmail.com>
2024-05-19 20:42:21 +0500

    3rd project

--- flutter_application_1/lib/main1.dart
+++ flutter_application_1/lib/main1.dart
@@ -5,100 +5,121 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  late TextEditingController _firstController;
+  late TextEditingController _secondController;
+  String _firstText = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _firstController = TextEditingController();
+    _secondController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _firstController.dispose();
+    _secondController.dispose();
+    super.dispose();
+  }
+
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("6-Amaliy ish"),
-          backgroundColor: Colors.green,
+          title: Text("6-amaliy ish"),
+          backgroundColor: Colors.yellow,
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CircleAvatar(
-              radius: 85,
+              radius: 65,
               backgroundImage: NetworkImage(
                   "https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/58f60527666431.56368c2348eca.jpg"),
             ),
             SizedBox(height: 12),
-            TextField(
-              decoration: InputDecoration(
-                suffixIcon: Icon(Icons.delete),
-                hintText: "Bu yerga yozishingiz mumkin!",
-                errorBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide(
-                    color: Colors.red,
-                    width: 3,
-                  ),  
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 56),
+              child: TextField(
+                controller: _firstController,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.pink,
+                  hintText: "Matn kiriting",
+                  contentPadding: EdgeInsets.symmetric(horizontal: 51),
                 ),
-                errorText: "Maydonni tuldiring",
+                onChanged: (text) {
+                  setState(() {
+                    _firstText = text;
+                  });
+                },
               ),
             ),
             SizedBox(height: 12),
             ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                "Yuborish",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 18,
-                ),
-                ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.green),
-                  foregroundColor: MaterialStateProperty.all(Color.fromARGB(194, 61, 89, 88)),
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  )),
-                ),
+              onPressed: () {
+                setState(() {
+                  _secondController.text = _firstText;
+                });
+              },
+              child: Text("Yuborish"),
             ),
+            SizedBox(height: 12),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36),
+              child: TextField(
+                controller: _secondController,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.blue,
+                  hintText: "Takrorlanuvchi matn",
+                  contentPadding: EdgeInsets.symmetric(horizontal: 26),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
           ],
         ),
         bottomNavigationBar: Theme(
-          data: ThemeData(
-              canvasColor: Color(0xFF1F1F1F),
+          data: ThemeData(canvasColor: Colors.black54),
+          child: BottomNavigationBar(
+            backgroundColor: Colors.black54,
+            iconSize: 24,
+            currentIndex: currentIndex,
+            onTap: (index) {
+              setState(() {
+                currentIndex = index;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_filled),
+                label: "Home",
               ),
-              child: BottomNavigationBar(
-                backgroundColor: Color.fromARGB(139, 30, 30, 30),
-                fixedColor: Colors.blue,
-                showSelectedLabels: true,
-                showUnselectedLabels: true,
-                iconSize: 24,
-                currentIndex: 2,
-                type: BottomNavigationBarType.fixed,
-                unselectedItemColor: Colors.red,
-                items: [
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.home_filled),
-                    label: "Home",
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.wallet),
-                    label: "Wallet",
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.pie_chart),
-                    label: "Pie Chart",
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.person),
-                    label: "Person",
-                  ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.wallet),
+                label: "Wallet",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.pie_chart),
+                label: "Pie chart",
+              ),
             ],
-          ),  
+          ),
         ),
       ),
-    );
+    ); 
   }
-}
+}           
